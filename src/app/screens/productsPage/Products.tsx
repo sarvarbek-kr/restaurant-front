26e608d772a56fe962d885c9c12d231f96f7fc86
Walter <sarvarbek200028@gmail.com>
2025-02-20 02:19:25 +0900

    feat: branch and other changes

--- src/app/screens/productsPage/Products.tsx
+++ src/app/screens/productsPage/Products.tsx
@@ -230,11 +230,9 @@ const history = useHistory();
                                     e.stopPropagation();
                                    }}
                                 >
-                                    <img 
-                                    alt=""
-                                    src={"/icons/shopping-cart.svg"}
-                                    style={{ display: "flex "}}
-                                    />
+                                    <b>
+                                        Order Now
+                                    </b>
                                 </Button>
                                 <Button className={"view-btn"} sx={{ right: "36px"}}>
                                     <Badge 
@@ -293,45 +291,23 @@ const history = useHistory();
 
 
       <div className={"brands-logo"}>
-        {/* <Container className={"family-brands"}>
-            <Box className={"category-title"}>Meet our Chef</Box>
-            <Stack
-          className={"events-info swiper-wrapper"}
-          spaceBetween={30}
-        >
-              <Stack className={"events-info-frame"}>
-                <div className={"events-img"}>
-                  <img src={value.img} className={"events-img"} />
-                </div>
-                <Box className={"events-desc"}>
-                  <Box className={"events-bott"}>
-                    <Box className={"bott-left"}>
-                      <div className={"event-title-speaker"}>
-                        <strong>{value.title}</strong>
-                        <div className={"event-organizator"}>
-                          <img src={"/icons/id-badge-regular.svg"} />
-                          <p className={"spec-text-author"}>{value.author}</p>
-                        </div>
-                      </div>
-
-                      <p className={"text-desc"}> {value.desc} </p>
-
-                      <div className={"bott-info"}>
-                        <div className={"bott-info-main"}>
-                          <img src={"/icons/calendar.svg"} />
-                          {value.date}
-                        </div>
-                        <div className={"bott-info-main"}>
-                          <img src={"/icons/location.svg"} />
-                          {value.location}
-                        </div>
-                      </div>
-                    </Box>
-                  </Box>
+      <Container className={"family-brands"}>
+            <Box className={"category-title"}>Our Branches</Box>
+            <Stack className={"brand-list"}>
+                <Box className={"review-box"}>
+                    <img src={"/img/gustoo.png"} alt="" />
                 </Box>
-              </Stack>
-        </Stack>
-        </Container> */}
+                <Box className={"review-box"}>
+                    <img src={"/img/gusto2.png"} alt=""/>
+                </Box>
+                <Box className={"review-box"}>
+                <img src={"/img/gusto3.png"} alt=""/>
+                </Box>
+                <Box className={"review-box"}>
+                <img src={"/img/gusto1.png"} alt=""/>
+                </Box>    
+            </Stack>
+        </Container>
       </div>
       <div className={"address"}>
         <Container>
