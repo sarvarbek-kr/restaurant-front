86600f9c87582c96f2dd61c24b235ca460ca6cc5
Walter <sarvarbek200028@gmail.com>
2025-07-20 10:51:48 +0500

    feat: develop socket.io Client

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './app/App';
-import reportWebVitals from './reportWebVitals';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './app/MaterialTheme';
-import './css/index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./app/App";
+import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./app/MaterialTheme";
+import "./css/index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import ContextProvider from './app/context/ContextProvider';
+import ContextProvider from "./app/context/ContextProvider";
+import { SocketProvider } from "./app/context/SocketContext";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -18,17 +19,18 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ContextProvider>
+        <SocketProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Router> 
+            <Router>
               <App />
-            </Router> 
+            </Router>
           </ThemeProvider>
-      </ContextProvider> 
+        </SocketProvider>
+      </ContextProvider>
     </Provider>
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
